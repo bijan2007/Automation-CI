c81e5efa8561fe613dc4742f3b3926ab1508fb33
bijan <bijan2007@gmail.com>
2017-03-16 22:38:16 +0530

    negative tests

--- src/test/java/utility/Constant.java
+++ src/test/java/utility/Constant.java
@@ -3,7 +3,7 @@ package utility;
 public class Constant {
 	public static final String URL = "https://staging.binary.com";
 	public static final String Email = "bijan+vi@binary.com";
-	public static final String Password = "Test1234";
+	public static final String Password = "est1234";
 	public static final String select_value = "";
 	public static final String fx_trading_exerience = "Over 3 years";
 	public static final String fx_trading_frequency = "40 transactions or more in the past 12 months";
