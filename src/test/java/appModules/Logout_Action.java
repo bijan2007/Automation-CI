0a8aa74d56ec1e731a0f9d57ec6c6fee18a58084
bijan <bijan2007@gmail.com>
2017-03-17 09:21:35 +0530

    Add Assert statements

--- src/test/java/appModules/Logout_Action.java
+++ src/test/java/appModules/Logout_Action.java
@@ -1,10 +1,12 @@
 package appModules;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 import pageObjects.MainAccount_Menu;
 public class Logout_Action {
 	public static void Execute(WebDriver driver) throws Exception {
+		Assert.assertTrue(driver.getTitle().matches("Trusted by traders.*"));
 		MainAccount_Menu.link_MainAccount(driver).click();
 		MainAccount_Menu.link_Logout(driver).click();
 	}
